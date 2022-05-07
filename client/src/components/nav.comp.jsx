d0c1b00053a783091c4168da937c030f8109dadd
DevArtist254 <kevinwasonga415@outlook.com>
2022-05-07 21:33:43 +0300

    ux-nav

--- client/src/components/nav.comp.jsx
+++ client/src/components/nav.comp.jsx
@@ -12,11 +12,13 @@ function Nav({ currentUser }) {
 
   return (
     <div className="nav">
-      <h3 className="nav__logo logo">Xplorer</h3>
+      <Link to="/" className="nav__logo logo">
+        Xplorer
+      </Link>
       <ul className="nav__list">
         <li className="nav__items">
           <span className="nav__search">
-            <input className="nav__search--input" />
+            <h2 className="nav__header">Find your next adventure</h2>
             <Icon
               className="iconify"
               icon="fluent:globe-search-24-filled"
@@ -24,58 +26,85 @@ function Nav({ currentUser }) {
             />
           </span>
           <img src={triangle} alt="tri" className="triangle" />
-          <div className="dropdown">
-            <h2 className="dropdown__header mt2">Filters</h2>
+          <div className="dropdown" style={{ left: '5%' }}>
+            <h2 className="dropdown__header mt2">Find by</h2>
 
-            <div className="dropdown__diff mt2">
-              <div className="dropdown__diff--input">
-                <input type="checkbox" />
-                <h3 className="dropdown__diff--header">Difficulty</h3>
-              </div>
-              <div className="dropdown__diff--input">
-                <input type="checkbox" />
-                <h3 className="dropdown__diff--header">Medium</h3>
-              </div>
-              <div className="dropdown__diff--input">
-                <input type="checkbox" />
-                <h3 className="dropdown__diff--header">Easy</h3>
-              </div>
+            <div
+              className="dropdown__cta mt2"
+              onClick={() => navRoute('/search/by-name')}
+            >
+              Name
             </div>
-
-            <div className="dropdown__range mt2">
-              <h3 className="dropdown__range--header">Range</h3>
-
-              <div className="dropdown__range--inputs">
-                <input type="text" className="dropdown__range--input" />
-                <p className="dropdown__range--p">to</p>
-                <input type="text" className="dropdown__range--input" />
-              </div>
+            <div
+              className="dropdown__cta mt2"
+              onClick={() => navRoute('/search/by-price')}
+            >
+              Price
             </div>
-
-            <div className="dropdown__location mt2">
-              <span
+            <div
+              className="dropdown__cta mt2"
+              onClick={() => navRoute('/search/by-duration')}
+            >
+              Duration
+            </div>
+            <div
+              className="dropdown__cta mt2"
+              onClick={() => navRoute('/search/by-difficulty')}
+            >
+              Difficulty
+            </div>
+          </div>
+        </li>
+        <li className="nav__items">
+          <span className="nav__search">
+            <h2 className="nav__header">Bookings</h2>
+            <a href="/nav.html" className="nav__link">
+              <Icon
                 className="iconify"
-                icon="entypo:location"
-                style={{ color: 'white', fontSize: '3.5rem' }}
+                icon="bi:handbag-fill"
+                style={{ color: '#000', fontSize: '3.5rem' }}
               />
-              <p className="dropdown__location--header">Close to me</p>
+            </a>
+          </span>
+          <img src={triangle} alt="tri" className="triangle-2" />
+          <div className="dropdown">
+            <h2 className="dropdown__header">Tours</h2>
+            <div className="dropdown__tours">
+              <h4 className="dropdown__tours--header">Tour</h4>
+              <p className="dropdown__tours--total">$200</p>
+              <img src={x} alt="cancel" className="dropdown__tours--cancel" />
+            </div>
+            <div className="dropdown__tours">
+              <h4 className="dropdown__tours--header">Tour</h4>
+              <p className="dropdown__tours--total">$200</p>
+              <img src={x} alt="cancel" className="dropdown__tours--cancel" />
+            </div>
+            <div className="dropdown__tours">
+              <h4 className="dropdown__tours--header">Tour</h4>
+              <p className="dropdown__tours--total">$200</p>
+              <img src={x} alt="cancel" className="dropdown__tours--cancel" />
             </div>
 
-            <div className="dropdown__cta mt2">Submit</div>
+            <p className="dropdown__p">Total : Total amount</p>
+            <div className="dropdown__cta">Pay</div>
           </div>
         </li>
         <li className="nav__items">
           {!currentUser ? (
             <>
-              <Link to="/user" className="nav__link">
-                <Icon
-                  className="iconify"
-                  icon="healthicons:ui-user-profile"
-                  style={{ color: '#000', fontSize: '3.5rem' }}
-                />
-              </Link>
+              <span className="nav__search">
+                <h2 className="nav__header">you</h2>
+                <Link to="/user" className="nav__link">
+                  <Icon
+                    className="iconify"
+                    icon="healthicons:ui-user-profile"
+                    style={{ color: '#000', fontSize: '3.5rem' }}
+                  />
+                </Link>
+              </span>
+
               <img src={triangle} alt="tri" className="triangle-1" />
-              <div className="dropdown">
+              <div className="dropdown" style={{ right: '0' }}>
                 <div
                   className="dropdown__cta mt2"
                   onClick={() => navRoute('/login')}
@@ -108,7 +137,7 @@ function Nav({ currentUser }) {
                 )}
               </Link>
               <img src={triangle} alt="tri" className="triangle-1" />
-              <div className="dropdown">
+              <div className="dropdown" style={{ right: '0' }}>
                 <h2 className="dropdown__header mt2">User Profile</h2>
                 <h3 className="dropdown__header--sub mt2">Name</h3>
                 <h3 className="dropdown__p">{currentUser.fullName}</h3>
@@ -121,40 +150,6 @@ function Nav({ currentUser }) {
               </div>
             </>
           )}
-        </li>
-        <li className="nav__items">
-          <a href="/nav.html" className="nav__link">
-            <Icon
-              className="iconify"
-              icon="bi:handbag-fill"
-              style={{ color: '#000', fontSize: '3.5rem' }}
-            />
-          </a>
-          <img src={triangle} alt="tri" className="triangle-2" />
-          <div className="dropdown">
-            <h2 className="dropdown__header">Tours</h2>
-            <div className="dropdown__tours">
-              <h4 className="dropdown__tours--header">Tour</h4>
-              <p className="dropdown__tours--total">$200</p>
-              <img src={x} alt="cancel" className="dropdown__tours--cancel" />
-            </div>
-            <div className="dropdown__tours">
-              <h4 className="dropdown__tours--header">Tour</h4>
-              <p className="dropdown__tours--total">$200</p>
-              <img src={x} alt="cancel" className="dropdown__tours--cancel" />
-            </div>
-            <div className="dropdown__tours">
-              <h4 className="dropdown__tours--header">Tour</h4>
-              <p className="dropdown__tours--total">$200</p>
-              <img src={x} alt="cancel" className="dropdown__tours--cancel" />
-            </div>
-
-            <p className="dropdown__p">Total : Total amount</p>
-            <div className="dropdown__cta">Pay</div>
-          </div>
-        </li>
-        <li className="nav__items">
-          <img src={wishlist} alt="wish list" style={{ width: '110px' }} />
         </li>
       </ul>
     </div>
